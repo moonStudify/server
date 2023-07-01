682aaeb3efcef1e21bbb33f0f89236fb1ac67838
TienNHM <TienNHM.it@gmail.com>
2023-07-02 00:27:02 +0700

    allow all site

--- index.js
+++ index.js
@@ -31,19 +31,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
 
-app.use(cors());
-// whitelist cors
-const whitelist = ['http://localhost:3000', 'http://localhost:5000', hostUrl]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -53,8 +45,8 @@ app.use(
   swaggerUi.setup(
     swaggerFile,
     {
-      swaggerOptions: { 
-        persistAuthorization: true 
+      swaggerOptions: {
+        persistAuthorization: true
       }
     }
   )
